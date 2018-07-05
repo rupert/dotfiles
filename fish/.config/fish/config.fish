f10805f9722b4f57fa4aba3e6cab4557b46cf71b
Rupert Bedford <rupert@rupertb.com>
2018-07-05 13:21:47 +0100

    Add rg config

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -3,6 +3,7 @@ set fish_greeting ""
 set -gx PYTHONPATH ~/Library/Python/3.7/lib/python/site-packages $PYTHONPATH
 set -gx PROJECT_HOME ~/Developer
 set -gx PATH $PATH ~/.local/bin ~/Library/Python/3.7/bin
+set -gx RIPGREP_CONFIG_PATH ~/.config/rg
 
 eval (direnv hook fish)
 eval (python3 -m virtualfish auto_activation projects)
