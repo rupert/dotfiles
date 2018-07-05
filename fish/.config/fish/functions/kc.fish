c3a5114a041ce96e1f511bff83071fb558461988
Rupert Bedford <rupert@rupertb.com>
2018-07-06 00:45:32 +0100

    Tabs to spaces

--- fish/.config/fish/functions/kc.fish
+++ fish/.config/fish/functions/kc.fish
@@ -1,7 +1,7 @@
 function kc --description 'Switch between Kubernetes contexts'
-	if count $argv >/dev/null
-		kubectl config use-context $argv[1]
-	else
-		kubectl config current-context
-	end
+    if count $argv >/dev/null
+        kubectl config use-context $argv[1]
+    else
+        kubectl config current-context
+    end
 end
