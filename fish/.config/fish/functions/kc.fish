ed2efe35c27f49792b2c49d0c254391b15773b2a
Rupert Bedford <rupert@rupertb.com>
2018-07-06 21:52:12 +0100

    Rewrite kc in bash

--- fish/.config/fish/functions/kc.fish
+++
@@ -1,7 +0,0 @@
-function kc --description 'Switch between Kubernetes contexts'
-    if count $argv >/dev/null
-        kubectl config use-context $argv[1]
-    else
-        kubectl config current-context
-    end
-end
