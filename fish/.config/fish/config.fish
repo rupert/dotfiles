dba557ae7d179c9cf04c95727661f8817aa0b215
Rupert Bedford <rupert@rupertb.com>
2018-08-30 23:37:39 +0100

    Add cargo to path

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -2,7 +2,7 @@ set fish_greeting ""
 
 set -gx PYTHONPATH ~/Library/Python/3.7/lib/python/site-packages $PYTHONPATH
 set -gx PROJECT_HOME ~/Developer
-set -gx PATH $PATH ~/.local/bin ~/Library/Python/3.7/bin ~/Developer/dotfiles/scripts
+set -gx PATH $PATH ~/.cargo/bin ~/.local/bin ~/Library/Python/3.7/bin ~/Developer/dotfiles/scripts
 set -gx RIPGREP_CONFIG_PATH ~/.config/rg
 
 eval (direnv hook fish)
