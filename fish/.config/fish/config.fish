8b4897296d45f1f5e66f6cd03a86aba5b2dd7e5e
Rupert Bedford <rupert@rupertb.com>
2018-08-30 23:37:51 +0100

    Add chrome alias

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -5,5 +5,7 @@ set -gx PROJECT_HOME ~/Developer
 set -gx PATH $PATH ~/.cargo/bin ~/.local/bin ~/Library/Python/3.7/bin ~/Developer/dotfiles/scripts
 set -gx RIPGREP_CONFIG_PATH ~/.config/rg
 
+alias chrome "/Applications/Google\ Chrome.app/Contents/MacOS/Google\ Chrome"
+
 eval (direnv hook fish)
 eval (/usr/local/bin/python3 -m virtualfish auto_activation projects)
