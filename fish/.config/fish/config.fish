cf056a4094d63a9b10855d323b635e0830fd642d
Rupert Bedford <rupert@rupertb.com>
2018-09-25 20:37:16 +0100

    Tidy up fish config

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -1,9 +1,9 @@
 set fish_greeting ""
 
-set -gx PYTHONPATH ~/Library/Python/3.7/lib/python/site-packages $PYTHONPATH
-set -gx PROJECT_HOME ~/Developer
-set -gx PATH $PATH ~/.cargo/bin ~/.local/bin ~/Library/Python/3.7/bin ~/Developer/dotfiles/scripts
-set -gx RIPGREP_CONFIG_PATH ~/.config/rg
+set -x PYTHONPATH ~/Library/Python/3.7/lib/python/site-packages $PYTHONPATH
+set -x PROJECT_HOME ~/Developer
+set -x PATH $PATH ~/.cargo/bin ~/.local/bin ~/Library/Python/3.7/bin ~/Developer/dotfiles/scripts
+set -x RIPGREP_CONFIG_PATH ~/.config/rg
 
 alias chrome "/Applications/Google\ Chrome.app/Contents/MacOS/Google\ Chrome"
 
@@ -11,5 +11,5 @@ eval (direnv hook fish)
 eval (/usr/local/bin/python3 -m virtualfish auto_activation projects)
 
 set PYENV_ROOT $HOME/.pyenv
-set -x PATH $PYENV_ROOT/shims $PYENV_ROOT/bin $PATH
+set -x PATH $PYENV_ROOT/shims $PATH
 pyenv rehash
