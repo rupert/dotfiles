c7f57da830a5076edb23c8d98c0dc74e8a153ddb
Rupert Bedford <182958+rupert@users.noreply.github.com>
2022-01-12 18:08:30 +0000

    Add starship

--- install.sh
+++ install.sh
@@ -4,6 +4,7 @@ set -euo pipefail
 
 GIT_DELTA_VERSION=0.11.3
 GITHUB_CLI_VERSION=2.4.0
+STARSHIP_VERSION=1.1.1
 
 tmp=$(mktemp -d)
 
@@ -20,8 +21,14 @@ function download() {
 
 trap cleanup EXIT
 
+cd "$tmp"
+
 download "https://github.com/dandavison/delta/releases/download/${GIT_DELTA_VERSION}/git-delta_${GIT_DELTA_VERSION}_amd64.deb" "$tmp/git-delta.deb"
-dpkg -i "$tmp/git-delta.deb"
+dpkg -i "git-delta.deb"
 
 download "https://github.com/cli/cli/releases/download/v${GITHUB_CLI_VERSION}/gh_${GITHUB_CLI_VERSION}_linux_amd64.deb" "$tmp/gh.deb"
-dpkg -i "$tmp/gh.deb"
+dpkg -i "gh.deb"
+
+download "https://github.com/starship/starship/releases/download/v${STARSHIP_VERSION}/starship-x86_64-unknown-linux-gnu.tar.gz" "$tmp/starship.tar.gz"
+tar -xzf "starship.tar.gz"
+mv starship /usr/local/bin
