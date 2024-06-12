17aa36a7ed040edf13d0a3adc82ff1fdf4a1d315
Rupert Bedford <182958+rupert@users.noreply.github.com>
2024-06-12 17:50:39 +0100

    Upgrade SST

--- sst.nix
+++ sst.nix
@@ -2,11 +2,11 @@
 
 pkgs.stdenv.mkDerivation {
   name = "sst";
-  version = "0.0.329";
+  version = "0.0.419";
 
   src = pkgs.fetchzip {
-    url = "https://github.com/sst/ion/releases/download/v0.0.329/sst-mac-arm64.tar.gz";
-    hash = "sha256-goGoUEXOIJLbDm2BRwy0+NBgjykZdek+aGxyyDaJEps=";
+    url = "https://github.com/sst/ion/releases/download/v0.0.419/sst-mac-arm64.tar.gz";
+    hash = "sha256-iuSNf3EuvLaTecjdZAoH1mX6vTltwa5g93kM9gPddGI=";
     stripRoot = false;
   };
 
