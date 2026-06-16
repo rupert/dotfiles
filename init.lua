df3752305652685cb508bb4846a63e609185f3e2
Rupert Bedford <182958+rupert@users.noreply.github.com>
2026-06-16 22:35:29 +0100

    Use LSP for gd and gD

--- init.lua
+++ init.lua
@@ -33,6 +33,9 @@ vim.api.nvim_create_autocmd('LspAttach', {
   callback = function(event)
     local client = assert(vim.lsp.get_client_by_id(event.data.client_id))
 
+    vim.keymap.set('n', 'gd', vim.lsp.buf.definition, { buffer = event.buf })
+    vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, { buffer = event.buf })
+
     if client.name == "eslint" then
       vim.api.nvim_create_autocmd("BufWritePre", {
         group = lsp_group,
