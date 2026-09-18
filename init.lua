81e6b3b6bfb668628d1c40cc56085ebbc704790b
Rupert Bedford <182958+rupert@users.noreply.github.com>
2026-09-18 21:39:34 +0100

    Add relative line numbers

--- init.lua
+++ init.lua
@@ -1,5 +1,7 @@
 vim.g.mapleader = " "
 vim.opt.clipboard = "unnamedplus"
+vim.opt.number = true
+vim.opt.relativenumber = true
 
 require("blink.cmp").setup(
   { keymap = { preset = "super-tab" } } --[[@as blink.cmp.Config]]
