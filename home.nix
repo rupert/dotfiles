a8539713d1487b92e5ff79a83b9ad97dc09272f1
Rupert Bedford <182958+rupert@users.noreply.github.com>
2026-09-25 10:38:33 +0100

    Add lazygit tokyonight theme

--- home.nix
+++ home.nix
@@ -239,6 +239,35 @@
     options.navigate = true;
   };
 
+  programs.lazygit = {
+    enable = true;
+
+    settings.gui = {
+      nerdFontsVersion = "3";
+
+      # https://github.com/folke/tokyonight.nvim/blob/cdc07ac78467a233fd62c493de29a17e0cf2b2b6/extras/lazygit/tokyonight_night.yml
+      theme = {
+        activeBorderColor = [
+          "#ff9e64"
+          "bold"
+        ];
+        inactiveBorderColor = [ "#27a1b9" ];
+        searchingActiveBorderColor = [
+          "#ff9e64"
+          "bold"
+        ];
+        optionsTextColor = [ "#7aa2f7" ];
+        selectedLineBgColor = [ "#283457" ];
+        cherryPickedCommitFgColor = [ "#7aa2f7" ];
+        cherryPickedCommitBgColor = [ "#bb9af7" ];
+        markedBaseCommitFgColor = [ "#7aa2f7" ];
+        markedBaseCommitBgColor = [ "#e0af68" ];
+        unstagedChangesColor = [ "#db4b4b" ];
+        defaultFgColor = [ "#c0caf5" ];
+      };
+    };
+  };
+
   programs.starship = {
     enable = true;
 
